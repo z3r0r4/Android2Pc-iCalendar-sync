1342b99f258672a5aeb02c8874663dce5131958e
unknown <R4C1.meru@protonmail.com>
2019-09-01 17:47:07 +0200

    improved csv2ics parser

--- a2p-ics-sync-win.ps1
+++ a2p-ics-sync-win.ps1
@@ -1,18 +1,17 @@
-Write-Host "Pull cal as csv from phone using adb-export"
+Write-Host "Pull cal as csv from phone using adb-export.sh then parse it using csv2ics.py"
 $currentwsldir = wsl wslpath -a $($(Convert-Path .) -replace '\\', '\\')
 $scriptwsldir = $currentwsldir+"/lib/adb-export/adb-export.sh"
-if (($storagewsldir = Read-Host "Input WINDOWSPath were to store ics calendar Press enter to accept default value $currentwsldir") -eq '') {$storagewsldir=$currentwsldir} else {$storagewsldir = wsl wslpath -a $storagewsldir}
+if (($storagewsldir = Read-Host "Input WINDOWS-Path were to store ics calendar (Press enter to accept default value $currentwsldir)") -eq '') {$storagewsldir=$currentwsldir} else {$storagewsldir = wsl wslpath -a $storagewsldir}
+$calendarName = Read-Host "Name of Calendar that should be exported"
 
 Write-Host "storagewsldir is: $storagewsldir"
 Write-Host "scriptwsldir is: $scriptwsldir
 "
-Write-Host "Executing Export" -BackgroundColor Blue
+Write-Host "Pulling Calendars from Phone" -BackgroundColor Blue
 
-#bash -c "cd $storagewsldir && echo 'storagewsldir is:' && pwd && $scriptwsldir -e content://com.android.calendar/events "
-
-
+bash -c "cd $storagewsldir && echo 'storagewsldir is:' && pwd && $scriptwsldir -e content://com.android.calendar/events "
 
 $storagedir = wsl wslpath -w $storagewsldir
-$mostRecentCal = gci $storagedir\com.android.calendar | ? { $_.PSIsContainer } | sort CreationTime -desc | select -f 1
-Write-Host "Parse pulled CSV Calendar to ICS Calendar"
-py .\csv2ics.py $storagedir\com.android.calendar\$mostRecentCal\data.csv $storagedir\exportedICalendar
+$mostRecentCal = gci $storagedir\com.android.calendar | ? { $_.PSIsContainer } | sort CreationTime -desc | select -f 1 
+Write-Host "Parse pulled CSV Calendar to ICS Calendar" -BackgroundColor Blue
+py .\csv2ics.py $storagedir\com.android.calendar\$mostRecentCal\data.csv $storagedir\exportedICalendar $calendarName
